f057d3a2f8f7c1ffd5500b5efa194e5bdda7ea4e
felix <felix.torres86@gmail.com>
2016-10-27 17:02:14 +0200

    circle_detector

---
+++ CMakeFiles/webcam_circles.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/webcam_circles.dir/src/circle_detector.cpp.o"
+  "webcam_circles.pdb"
+  "webcam_circles"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/webcam_circles.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
--- src/circle_detector.cpp
+++ src/circle_detector.cpp
@@ -30,11 +30,11 @@ int main(int argc, char *argv[])
     int radius;
     
 
-// 	cv::Ptr<cv::ORB> orb_detector = new cv::ORB(MIN_NUM_FEATURES); //ORB point feature detector
-//     cv::vector<cv::KeyPoint> point_set; //set of point features
-//     cv::Ptr<cv::DescriptorExtractor> orb_descriptor; //ORB descriptor
-//     orb_descriptor = cv::DescriptorExtractor::create("ORB"); //init the descriptor
-//     cv::Mat descriptor_set; //set of descriptors, for each feature there is an associated descriptor 
+ 	cv::Ptr<cv::ORB> orb_detector = new cv::ORB(10); //ORB point feature detector
+     cv::vector<cv::KeyPoint> point_set; //set of point features
+     cv::Ptr<cv::DescriptorExtractor> orb_descriptor; //ORB descriptor
+     orb_descriptor = cv::DescriptorExtractor::create("ORB"); //init the descriptor
+     cv::Mat descriptor_set; //set of descriptors, for each feature there is an associated descriptor 
 	
 	//check user args
 	switch(argc)
